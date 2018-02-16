3432cac4883119f02534516244e5dfa01b51043e
pneri <pcpneri@gmail.com>
2018-02-16 17:05:00 -0200

    criamos a classe Page

--- index.php
+++ index.php
@@ -1,40 +1,22 @@
-<<<<<<< HEAD
 <?php
 
 require_once("vendor/autoload.php");
 
-$app = new \Slim\Slim();
+use \Slim\Slim;
+use \Hcode\Page;
+
+$app = new Slim();
 
 $app->config('debug', true);
 
 $app->get('/', function() {
 
-	$sql = new Hcode\DB\Sql();
+	$page = new Page();
 
-	$results = $sql->select("SELECT * FROM tb_users");
-
-	echo json_encode($results);
-
+	$page->setTpl("index");
+	
 });
 
 $app->run();
 
-=======
-<?php
-
-require_once("vendor/autoload.php");
-
-$app = new \Slim\Slim();
-
-$app->config('debug', true);
-
-$app->get('/', function() {
-
-	echo "OK";
-
-});
-
-$app->run();
-
->>>>>>> ae5d4dd2265af32dab0e9bf51e4c49144cc1224a
 ?>
